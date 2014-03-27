8884075e3cd80de8458287af989f3ae7041fdc91
Kyle Dornblaser <kdornblaser@gmail.com>
2014-03-27 17:11:40 -0400

    Cleaning out old files

--- saved_restaurants.php
+++
@@ -1,4 +0,0 @@
-<?php include 'header.php'; ?>
-	<h2>Saved Restaurants and Bars</h2>
-	<p>Future feature</p>
-<?php include 'footer.php'; ?>
--- thanks.php
+++
@@ -1,3 +0,0 @@
-<?php include 'header.php'; ?>
-			<p>Thank you for submitting a drink!</p>
-<?php include 'footer.php'; ?>
